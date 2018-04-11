f15bb971fa53f9a8c7f24503340ebfc40c4412a2
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-04-11 17:55:30 +0100

    created app component, added header, edited styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import styles from './style.less';
 import './fonts/fontawesome.min.js';
 import './fonts/fa-brands.min.js';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      ThirtyDays: [],
+      allTime: []
+    }
+  }
+  render() {
+    return (
+      <header>freeCodeCamp <i className='fab fa-free-code-camp'></i></header>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('container'));
