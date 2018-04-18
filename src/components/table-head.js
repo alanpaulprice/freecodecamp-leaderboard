618c2579e4c2f7b3986492e522ebbdce10c20504
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-04-18 13:14:37 +0100

    temporary fix of update current function (achieved by removing icon)

--- src/components/table-head.js
+++ src/components/table-head.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const TableHead = (props) => {
 
-  const invertedIcon = <i className='fas fa-sort-amount-down fa-lg align-middle'></i>;
+  const sortedByIcon = <span>&#450;</span>;
+  //<i className='fas fa-sort-amount-down align-middle'></i>;
 
   const recentTableHead = props.current === 'recent' ?
-  <span>Points - Past 30 Days {invertedIcon}</span> :
+  <span>Points - Past 30 Days {sortedByIcon}</span> :
   <span>Points - Past 30 Days</span>;
 
   const allTimeTableHead = props.current === 'allTime' ?
-  <span>Points - All Time {invertedIcon}</span> :
+  <span>Points - All Time {sortedByIcon}</span> :
   <span>Points - All Time</span>;
 
   return (
@@ -27,11 +28,5 @@ const TableHead = (props) => {
 export default TableHead;
 
 /*
-const recentTableHead = props.current === 'recent' ?
-<span>Points - Past 30 Days {invertedIcon}</span> :
-<span>Points - Past 30 Days</span>;
-
-const allTimeTableHead = props.current === 'allTime' ?
-<span>Points - All Time {invertedIcon}</span> :
-<span>Points - All Time</span>;
+latin letter &#450;
 */
