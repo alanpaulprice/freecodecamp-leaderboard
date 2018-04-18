5628d8af0a5c498fb0ceea5ff4ca2104cb821bcb
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-04-18 16:31:12 +0100

    added fcc profile anchor tags to usernames and avatars

--- src/components/table-body.js
+++ src/components/table-body.js
@@ -7,8 +7,8 @@ const TableBody = (props) => {
       return (
         <tr key={index + user}>
           <td className="align-middle">{index + 1}</td>
-          <td className="align-middle"><img className='avatar' src={user.img}/></td>
-          <td className="align-middle">{user.username}</td>
+          <td className="align-middle"><a href={'https://www.freecodecamp.org/' + user.username} target='_blank' ><img className='avatar' src={user.img}/></a></td>
+          <td className="align-middle td-user-username"><a href={'https://www.freecodecamp.org/' + user.username} target='_blank' >{user.username}</a></td>
           <td className="align-middle">{user.recent}</td>
           <td className="align-middle">{user.alltime}</td>
         </tr>
