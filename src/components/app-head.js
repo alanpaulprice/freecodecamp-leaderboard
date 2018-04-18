bbb76fc6cd32209facf4c86db991d998cebccdb8
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-04-18 15:37:35 +0100

    added page reload on app head click

--- src/components/app-head.js
+++ src/components/app-head.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppHeader = () => {
   return (
     <header className='p-1 pl-3'>
-      <h2 className='text-center'>
+      <h2 className='text-center' onClick={() => window.location.reload()}>
         freeCodeCamp&nbsp;
         <i className='fab fa-free-code-camp fa-lg align-middle'/>
         &nbsp;leaderboard
